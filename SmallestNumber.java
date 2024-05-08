b7c535fa0da06af2ac8ca75d2252e3219c51bd15
keerthanakalaiselvan05 <161156415+keerthanakalaiselvan05@users.noreply.github.com>
2024-05-08 13:14:10 +0530

    Updated the SmallestNumber.java file

--- SmallestNumber.java
+++ SmallestNumber.java
@@ -3,8 +3,14 @@ package com.guvi.task1;
 public class SmallestNumber {
 
 	public static void main(String[] args) {
-		
-
+		//check the number is positive or negative
+		System.out.println("Enter a number : ");
+		Scanner Sc=new Scanner(System.in);
+		int num=Sc.nextInt();
+		if(num>=0)
+			System.out.println("Given number is Positive");
+		else
+			System.out.println("Given number is Negative");
+		Sc.close();
+		}
 	}
-
-}
