47d4c6eec48f5d639c0bfb13c6ef4d10c81e5917
keerthanakalaiselvan05 <161156415+keerthanakalaiselvan05@users.noreply.github.com>
2024-05-08 13:17:04 +0530

    Made changes in checking the condition

--- PositiveNegative.java
+++ PositiveNegative.java
@@ -8,7 +8,7 @@ public class PositiveNegative {
 		System.out.println("Enter a number : ");
 		Scanner Sc=new Scanner(System.in);
 		int num=Sc.nextInt();
-		if(num>0)
+		if(num>=0)
 			System.out.println("Given number is Positive");
 		else
 			System.out.println("Given number is Negative");
